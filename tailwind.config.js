f5d35685857c2c482b44c87511f393ccff30159a
Attaftazayn <123521976+Attaftazayn@users.noreply.github.com>
2023-09-11 07:15:05 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  darkMode:"class",
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      spacing:{
+        "big":"32rem"
+      }
+    },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+    },
+    fontFamily:{
+      poppins:['Poppins', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
+
